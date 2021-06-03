5f44bf0e10c2258253c28bc9c8b71fef8d79bb3d
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2021-06-03 13:15:15 +0200

    added migration for user data table

--- database/migrations/2021_05_31_191427_create_user_data_table.php
+++ database/migrations/2021_05_31_191427_create_user_data_table.php
@@ -15,6 +15,19 @@ class CreateUserDataTable extends Migration
     {
         Schema::create('user_data', function (Blueprint $table) {
             $table->id();
+            $table->string('citizenship')->nullable();
+            $table->string('passport_num')->nullable();
+            $table->string('passport_issuer')->nullable();
+            $table->string('issuer_code')->nullable();
+            $table->string('issue_date')->nullable();
+            $table->string('user_address')->nullable();
+            $table->string('inn')->nullable();
+            $table->string('passport_photo_first')->nullable();
+            $table->string('passport_photo_address')->nullable();
+            $table->string('code_kaz')->nullable();
+            $table->string('personal_code')->nullable();
+            $table->string('iban')->nullable();
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
         });
     }
