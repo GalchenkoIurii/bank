5532d63808dbb7d531a861415c3b01bbd864bd7f
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2021-06-03 12:24:50 +0200

    added migrations for credits and credit settings tables

--- database/migrations/2021_05_31_185845_create_credits_table.php
+++ database/migrations/2021_05_31_185845_create_credits_table.php
@@ -15,6 +15,19 @@ class CreateCreditsTable extends Migration
     {
         Schema::create('credits', function (Blueprint $table) {
             $table->id();
+            $table->unsignedInteger('credit_setting_id');
+            $table->double('credit_sum');
+            $table->unsignedSmallInteger('credit_term');
+            $table->double('percent');
+            $table->double('monthly_payment');
+            $table->unsignedInteger('user_id');
+            $table->string('full_name')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('email')->nullable();
+            $table->unsignedTinyInteger('reviewing')->default(1);
+            $table->unsignedTinyInteger('result')->default(0);
+            $table->text('credit_agreement')->nullable();
+            $table->text('payments_table')->nullable();
             $table->timestamps();
         });
     }
--- database/migrations/2021_05_31_185925_create_credit_settings_table.php
+++ database/migrations/2021_05_31_185925_create_credit_settings_table.php
@@ -14,7 +14,19 @@ class CreateCreditSettingsTable extends Migration
     public function up()
     {
         Schema::create('credit_settings', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('category_lt')->nullable();
+            $table->string('category_en')->nullable();
+            $table->string('category_ru')->nullable();
+            $table->string('category_slug');
+            $table->text('description_lt')->nullable();
+            $table->text('description_en')->nullable();
+            $table->text('description_ru')->nullable();
+            $table->double('min_sum');
+            $table->double('max_sum');
+            $table->unsignedSmallInteger('min_term');
+            $table->unsignedSmallInteger('max_term');
+            $table->double('percent');
             $table->timestamps();
         });
     }
